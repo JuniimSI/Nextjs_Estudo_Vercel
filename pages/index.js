261524f6db0f62a81d9610d7622b498237ec8c66
Francisco Fernandes Júnior <jrfernandesti@gmail.com>
2021-01-18 21:28:54 -0300

    novo main

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,9 @@ function Home() {
     return (
         <div>
             <div>Home</div>
-            <Contador />
         </div>
     );
 }
 
-function Contador() {
-    const [contador, setContador] = useState(1);
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-
-
-}
 
 export default Home;
