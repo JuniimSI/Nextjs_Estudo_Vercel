bc6e2dfb47db3da253d2838178a215e25d7fa5fc
Francisco Fernandes Júnior <jrfernandesti@gmail.com>
2021-01-19 21:30:26 -0300

    commit to vercel

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -4,7 +4,7 @@ async function tempo (req, res) {
     const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
     const subscribersResponseJson = await subscribersResponse.json();
     const inscritos = subscribersResponseJson.total_subscribers; 
-    console.log(inscritos)
+
     res.json({
         date: dynamicDate.toGMTString(),
         inscritos: inscritos
